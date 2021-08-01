136f818bf13d7db1902b992094f13c92f59641f1
Adaspeaks <adaspeaks@gmail.com>
2021-08-01 16:22:13 -0700

    added role questions

---
+++ src/internQuestions.js
@@ -0,0 +1,14 @@
+internQuestions = [
+    {
+        type: "input",
+        name:"school",
+        message:"What is this intern's school name??"
+    },
+    {
+        type:"confirm",
+        name:"reset",
+        message:"Do you want to add another team member?"
+    }
+]
+
+module.exports = internQuestions;
